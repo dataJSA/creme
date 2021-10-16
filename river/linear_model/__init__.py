8b82070798b40cf066c010fd6985a11596a93a66
Joshua Sant'Anna <45068597+dataJSA@users.noreply.github.com>
2021-10-16 15:38:07 +0200

    feat: Add NormalPrior class

--- river/linear_model/__init__.py
+++ river/linear_model/__init__.py
@@ -3,6 +3,7 @@ from .alma import ALMAClassifier
 from .glm import LinearRegression, LogisticRegression, Perceptron
 from .pa import PAClassifier, PARegressor
 from .softmax import SoftmaxRegression
+from .ad_predictor import NormalPrior
 
 __all__ = [
     "ALMAClassifier",
--- river/linear_model/ad_predictor.py
+++ river/linear_model/ad_predictor.py
@@ -3,23 +3,23 @@ from scipy import stats
 
 
 class NormalPrior(NamedTuple):
-    """Approximate Large Margin Algorithm (ALMA).
+    """Normal Prior over model weight.
+
+    The Normal Prior represents a gaussian belief over a weight i.e. weight follow normal distribution with parameters μ ``mean`` and σ2 ``variance``.
 
     Parameters
     ----------
     beta
-        Scale of the inverse link function (standard normal cumulative distribution function.
+        Scale of the inverse link function (standard normal cumulative distribution function).
     n_features
         Number of features.
     prior_probability
-        Prior probability on the feature weights prior_probability = P(y=1 | x, weights).
+        Prior probability on the feature weights.
 
     Attributes
     ----------
-    mean : float
-        Mean of the gaussian belief over the weight.
-    k : int
-        The number of instances seen during training.
+    variance : float
+                Variance of the gaussian belief over the weight.
 
     Examples
     --------
@@ -29,10 +29,10 @@ class NormalPrior(NamedTuple):
 
     >>> X_y = datasets.Phishing()
 
-    >>> init = linear_model.adpredictor.NormalPrior(beta=0.05, n_features=X_y.n_features, prior_probability=0.6)
+    >>> prior = linear_model.ad_predictor.NormalPrior(beta=0.05, n_features=X_y.n_features, prior_probability=0.6)
 
-    >>> print(init)
-    Biased normal prior on weights 𝒩(μ=2.281, σ2=1.000)
+    >>> print(prior)
+    Biased normal prior on a weight 𝒩(μ=2.281, σ2=1.000)
 
     """
 
@@ -42,8 +42,21 @@ class NormalPrior(NamedTuple):
     prior_probability: Union[float, None] = None
 
     @property
-    def mean(self) -> float:
-        0 if self.prior_probability is None else stats.norm.ppf(self.prior_probability) * (self.beta ** 2 + self.n_features)
-    
+    def mean(self):
+        """
+        mean : float
+            Mean of the gaussian belief over the weight.
+        """
+        return (
+            0
+            if self.prior_probability is None
+            else stats.norm.ppf(self.prior_probability)
+            * (self.beta ** 2 + self.n_features)
+        )
+
     def __repr__(self) -> str:
-        return f'Unbiased normal prior on weights 𝒩(μ={self.mean:.3f}, σ2={self.variance:.3f})' if self.prior_probability is None else f'Biased normal prior on weights 𝒩(μ={self.mean:.3f}, σ2={self.variance:.3f})'
+        return (
+            f"Unbiased normal prior on weights 𝒩(μ={self.mean:.3f}, σ2={self.variance:.3f})"
+            if self.prior_probability is None
+            else f"Biased normal prior on weights 𝒩(μ={self.mean:.3f}, σ2={self.variance:.3f})"
+        )
